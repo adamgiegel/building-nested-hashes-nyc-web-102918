432ef9490bc87026253637c518bceff32ed49e50
Adam Giegel <adam.giegel@gmail.com>
2018-10-19 15:45:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {	    :montague => {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
       }, 	      
-    },
-    :capulet => {	    :capulet => {
+    :capulet => {	  
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends =>[]
-      #your key/value pairs here!	      #your key/value pairs here!
-      }}	      }
+      },
     }
 end
